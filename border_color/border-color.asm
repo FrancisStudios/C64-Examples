47ba8c321e5300b6591933ebb73c515ba722e373
Francis <vlfmap@gmail.com>
2025-08-20 16:35:38 +0200

    comment type changed

--- border_color/border-color.asm
+++ border_color/border-color.asm
@@ -1,10 +1,9 @@
-//  Simplest way to change the border 53280 (or $d020)
+//; Simplest way to change the border 53280 (or $d020)
 
 BasicUpstart2(main)
-
 main:
 
-    lda #5              // loads the actual number (#) 5 into the accumulator
-    sta $d020           // mov lda, $d020 --> dumps lda into $d020 memory address.
+    lda #5              //; loads the actual number (#) 5 into the accumulator
+    sta $d020           //; mov lda, $d020 --> dumps lda into $d020 memory address.
 
-    rts                 // program terminates
+    rts                 //; program terminates
