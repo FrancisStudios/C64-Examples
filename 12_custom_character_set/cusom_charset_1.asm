d69222effc32fefbf533e6c85ac24a686b90a2fd
Francis <vlfmap@gmail.com>
2025-09-02 19:35:36 +0200

    charset example set-up

---
+++ 12_custom_character_set/cusom_charset_1.asm
@@ -0,0 +1,6 @@
+//; We have already explored the default character-set of the C64 
+//; in the first couple examples. https://sta.c64.org/cbm64scr.html
+//; We have printed them all in different colors and in monochrome mode too. 
+//; But what if I say that we can create custom charactersets to enhance our 
+//; applications' graphics.
+
